0890b27e67bedbff1b05d48340919a09f045abdf
Yngve Klakegg <klakegg@hotmail.com>
2018-07-11 13:45:23 +0200

    Added report for print of department. Added todos

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,10 @@
 //Scenario:
 //TODO: Interface for adding users manually
 //TODO: Add framework text interface (add, report, quit)
-//TODO: Redo if department doesn't exist
-//TODO: Print alphabetically (by lastname), by department. As function
-//TODO: "Add user" as function
-//TODO: Modulize
+//TODO: Create functions, to reduce code
+//TODO: Remove redundant code
+//TODO: Handle ids vs names for departments. Unnecessary steps to convert
+//TODO: Redo if department doesn't exist //read chapter 9 first
 
 #![allow(unused_variables)]
 // #[warn(dead_code)]
@@ -25,7 +25,6 @@ fn main() {
 
     //let mut departments_users: HashMap<String, u8> = HashMap::new();
     let mut departments_users: HashMap<String, &String> = HashMap::new();
-    
 
     println!(
         "There are {} new users in system, add these to correct department:",
@@ -80,19 +79,49 @@ fn main() {
 
     //print_string(print_operation);
     let mut print_company: Vec<_> = departments_users.iter().collect();
+    print_company.sort_by(|a, b| a.cmp(b));
 
     match print_operation.trim().as_ref() {
         "c" => {
-            println!("All users in company:", );
-            print_company.sort_by(|a, b| a.cmp(b));
+            println!("All users in company:",);
+
             print_company.iter().for_each(|&x| println!("{}", x.0))
         }
-        "d" => println!("All users by department printed"),
+        "d" => {
+            println!("Type which department too see users for:");
+            departments
+                .iter()
+                .for_each(|ref d| println!("Type {} for {}", d.dep_id, d.dep_name));
+
+            let mut input_string = String::new();
+
+            io::stdin()
+                .read_line(&mut input_string)
+                .expect("Failed to read line");
+
+            let department_to_print: u8 =
+                input_string.trim().parse::<u8>().expect("Wanted a number");
+
+            if !departments
+                .iter()
+                .any(|ref d| d.dep_id == department_to_print)
+            {
+                println!("Non-existing department, please add a valid department");
+            }
+            let department_name: &String = &departments
+                .iter()
+                .find(|d| d.dep_id == department_to_print)
+                .expect("")
+                .dep_name;
+            print_company.iter().filter(|&x|x.1 == &department_name).for_each(|&x| println!("{}", x.0));
+        }
         "cd" => {
             println!("All users in company, by department, sorted alphabetically: ");
             print_company.sort_by(|a, b| a.1.cmp(b.1).then_with(|| a.cmp(b)));
-            print_company.iter().for_each(|&x| println!("{}, {}", x.0, x.1))
-        },
+            print_company
+                .iter()
+                .for_each(|&x| println!("{}, {}", x.0, x.1))
+        }
         _ => (println!("LOL")),
     }
 } //main
@@ -135,7 +164,7 @@ fn initialize_users() -> Vec<User> {
     users.push(user2);
 
     users.push(User {
-        first_name: String::from("Andre"),
+        first_name: String::from("Andrè"),
         last_name: String::from("Agassi"),
         e_mail: String::from("aa@coolcorp.com"),
     });
